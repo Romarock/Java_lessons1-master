347bcb9324c45d005f104aa60bec352386a3d194
Romarock <55085243+Romarock@users.noreply.github.com>
2019-10-25 16:53:11 +0300

    Добавлен тест HbConnection

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -18,7 +18,7 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
 public Iterator<Object[]> validGroups() throws IOException {
-     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resourses/groups.xml")));
+     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
      String xml = "";
       String line = reader.readLine();
       while (line != null) {
