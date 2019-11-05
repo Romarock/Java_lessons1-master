7778b46fe15e65f03a6959bfd636cdc496f400ec
Romarock <55085243+Romarock@users.noreply.github.com>
2019-11-05 14:36:43 +0300

    Добавлено сравнение данных загруженных из бд для теста удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -18,10 +18,9 @@ public class GroupDeletionTests extends TestBase {
   public void ensurePreconditions() {
 
     app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
-
-      app.group().create(new GroupData().withName("test1"));
-
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test2"));
     }
 
   }
@@ -30,11 +29,11 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
 
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
     assertThat(app.group().count(), equalTo(before.size() - 1));
-    Groups after= app.group().all();
+    Groups after= app.db().groups();
     assertThat(after, equalTo(before.withOut(deletedGroup)));
 
 
