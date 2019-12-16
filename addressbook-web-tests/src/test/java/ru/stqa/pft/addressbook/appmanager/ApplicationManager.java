f7a57f16a67b2b0b3d0d47b4f5647d08841d9576
Roman Lobanov <Roman.Lobanov@t-systems.com>
2019-12-16 14:17:23 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,20 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -39,15 +43,25 @@ public class ApplicationManager {
 
         dbHelper = new DbHelper();
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-        wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)){
+        if ("".equals(properties.getProperty("selenium-server"))) {
 
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
 
-            wd = new InternetExplorerDriver();
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+
+           DesiredCapabilities  capabilities = new DesiredCapabilities();
+           capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium-server")), capabilities );
+
         }
+
 
        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
        wd.get(properties.getProperty("web.baseUrl"));
